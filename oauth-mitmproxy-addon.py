d80695f29b54f14de033a691f7a079a4190dba89
tothi <tothi@users.noreply.github.com>
2019-09-09 01:52:10 +0200

    add replay if token was refreshed (401) replace 401 response with replayed one
    Signed-off-by: tothi <tothi@users.noreply.github.com>

--- oauth-mitmproxy-addon.py
+++ oauth-mitmproxy-addon.py
@@ -10,16 +10,23 @@ class OAuthBurp:
         ctx.log(refresh_token)
         self.refresh_token, self.auth_token = ot.refresh_token(refresh_token)
         ctx.log("init refreshed authorization Bearer token")
-        
+
     def request(self, flow):
         flow.request.headers["authorization"] = "Bearer %s" % self.auth_token
         ctx.log("authorization Bearer token patched")
 
     def response(self, flow):
+        if flow.request.is_replay:
+            self._flow.response = flow.response
+            self._flow.resume()
+            return
         if flow.response.status_code == 401:
             self.refresh_token, self.auth_token = ot.refresh_token(self.refresh_token)
             ctx.log("refreshed authorization Bearer token")
-            # TODO: replay request
+            flow2 = flow.copy()
+            ctx.master.commands.call("replay.client", [flow2])
+            flow.intercept()
+            self._flow = flow
 
 addons = [
     OAuthBurp(ot.REFRESH_TOKEN)
--- oauth_template.py
+++ oauth_template.py
@@ -5,6 +5,7 @@ CLIENT_ID = "client_id"
 AUTH_URI = "https://example.com/api/authentication/"
 
 def refresh_token(refresh_token):
+    # in case of bad SSL certs, add: verify=False
     r = requests.post(AUTH_URI, data={"grant_type": "refresh_token", "refresh_token": refresh_token, "client_id": CLIENT_ID})
     assert r.status_code == 200
     assert r.json()['token_type'] == 'Bearer'
